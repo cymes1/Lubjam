c21d578b04aa7a9d66e00756a49ce37e8e3d5c16
KarolinaSerpatowska <4ka@wp.pl>
2019-01-26 00:55:10 +0100

    add menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void retMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+   public void exit()
+    {
+        Application.Quit();
+    }
+}
