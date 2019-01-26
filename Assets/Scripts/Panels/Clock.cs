b5db4b75f6720b778f582b242e36ad0a49b83e03
KarolinaSerpatowska <4ka@wp.pl>
2019-01-26 16:55:52 +0100

    clock

---
+++ Assets/Scripts/Panels/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    public int speed = 1;
+    public int time_ants_return=10;
+    
+    public GameObject arrow;
+    public GameObject anticon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrow.transform.RotateAround(arrow.transform.position, Vector3.back, speed * Time.deltaTime);
+
+        if(time_ants_return>2) anticon.SetActive(false);
+        else if(time_ants_return<=2) anticon.SetActive(true);
+    }
+
+
+}
