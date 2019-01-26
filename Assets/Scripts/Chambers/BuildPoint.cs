6bb0f6784bfe10dc7304ef9f4f91105e0e356432
cymes1 <mateusz.winiarczyk@outlook.com>
2019-01-26 03:53:44 +0100

    Add chamber construction

---
+++ Assets/Scripts/Chambers/BuildPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPoint : MonoBehaviour
+{
+    public enum Direction
+    {
+        LEFT,
+        RIGHT
+    }
+
+    public float offset = 1.1f;
+    public Direction direction;
+    public GameObject supplyChamberPrefab;
+
+    public void OnBuildButton()
+    {
+        Instantiate(supplyChamberPrefab, transform.position, Quaternion.identity);
+        transform.Translate((direction == Direction.LEFT ? -1 : 1) * offset, 0, 0);
+    }
+}
--- Assets/Scripts/Chambers/ConnectorChamber.cs
+++ Assets/Scripts/Chambers/ConnectorChamber.cs
@@ -6,4 +6,22 @@ public class ConnectorChamber : Chamber
 {
     public int level;
     public int price;
+    public GameObject leftSpawnPoint;
+    public GameObject rightSpawnPoint;
+    public GameObject connectorChamberPrefab;
+    public Transform connectorChamberSpawnPoint;
+
+    private bool isUnlocked;
+
+    public void Unlock()
+    {
+        //if(gameMaster.Resource >= price)
+        //{
+            isUnlocked = true;
+            leftSpawnPoint.SetActive(true);
+            rightSpawnPoint.SetActive(true);
+
+            Instantiate(connectorChamberPrefab, connectorChamberSpawnPoint.position, Quaternion.identity);
+        //}
+    }
 }
