55d837079bacf4f8d564c78fdf49ee410542344b
KarolinaSerpatowska <4ka@wp.pl>
2019-01-26 04:59:19 +0100

    add dialog expedition

--- Assets/Scripts/Chambers/ExpeditionChamber.cs
+++ Assets/Scripts/Chambers/ExpeditionChamber.cs
@@ -4,8 +4,30 @@ using UnityEngine;
 
 public class ExpeditionChamber : Chamber
 {
+    public GameObject dialogmenu;
+
     public void OnStartExpeditionButton()
     {
         Debug.Log("Start expedition");
+        dialogmenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Choose1Button()
+    {
+        //cos
+        dialogmenu.SetActive(false);
+    }
+
+    public void Choose2Button()
+    {
+        //cos
+        dialogmenu.SetActive(false);
+    }
+
+    public void ReturnButton()
+    {
+        Time.timeScale = 1;
+        dialogmenu.SetActive(false);
     }
 }
