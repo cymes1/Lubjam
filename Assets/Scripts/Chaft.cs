2273461bbd2dd06fec168e96c39a6ec1da99414e
cymes1 <mateusz.winiarczyk@outlook.com>
2019-01-27 13:37:24 +0100

    Build shafts

---
+++ Assets/Scripts/Chaft.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaft : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameMaster gameMaster;
+    public int price;
+
+    public void Build()
+    {
+        if(gameMaster.Resource >= price)
+        {
+            gameMaster.Resource -= price;
+            GetComponent<MeshRenderer>().enabled = false;
+            canvas.SetActive(false);
+        }
+    }
+}
