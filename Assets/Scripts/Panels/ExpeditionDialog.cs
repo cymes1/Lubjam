b9beea6fd199bf3c79ef527781fa2005bc29409e
Senitt <piotr.k.tor@gmail.com>
2019-01-27 03:22:37 +0100

    coś o 3:22

--- Assets/Scripts/Panels/ExpeditionDialog.cs
+++ Assets/Scripts/Panels/ExpeditionDialog.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class ExpeditionDialog : MonoBehaviour
 {
+    public GameObject expeditionPrefab;
+    public Transform expeditionSpawnPoint;
+    public GameMaster gameMaster;
+
     public void OnButton1()
     {
-        Debug.Log("OnButton1");
+        Expedition expedition = Instantiate(expeditionPrefab, expeditionSpawnPoint.position, expeditionSpawnPoint.rotation).GetComponent<Expedition>();
+        expedition.Init(Expedition.LootType.Food, 1, 10, gameMaster);
+        Hide();
     }
 
     public void OnButton2()
     {
-        Debug.Log("OnButton2");
+        Expedition expedition = Instantiate(expeditionPrefab, expeditionSpawnPoint.position, expeditionSpawnPoint.rotation).GetComponent<Expedition>();
+        expedition.Init(Expedition.LootType.Resource, 1, 10, gameMaster);
+        Hide();
     }
 
     public void OnExitButton()
+    {
+        Hide();
+    }
+
+    private void Hide()
     {
         gameObject.SetActive(false);
         Time.timeScale = 1;
